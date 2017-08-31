3c673713cb6c388bc4ab8754a4db5c178251f0c0
unknown <tgf1259@ad.tgfoshan.com.cn>
2017-08-31 11:19:15 +0800

    first commit

---
+++ application/index/controller/Login.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+use app\index\model\User as LoginModel;
+use think\View;
+
+class Login extends Controller
+{
+
+    public function index()
+    {
+        $view = new View();
+        return $view->fetch('index');
+
+    }
+
+    public function login()
+    {
+        $username=input('username');
+        $password=input('password');
+        $user=LoginModel::get([
+           'username'=>$username,
+            'password'=>$password
+        ]);
+        if($user)
+        {
+            return $this->success('登录成功'.$username.'     '.$password);
+        }else{
+            return $this->error('登录失败');
+        }
+    }
+}
